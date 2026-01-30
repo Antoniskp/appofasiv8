e2fa198a86c95be0faf1dc309de8b39f0d24a638
Antonis <antoniskp@gmail.com>
2026-01-30 19:44:22 +0200

    Update headings in HomeHero component

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -7,9 +7,12 @@ export default function HomeHero() {
   return (
     <section className="bg-blue-600 text-white py-20">
       <div className="app-container text-center">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">
-          Η πρώτη ιστοσελίδα ψηφιακών δημοσκοπήσεων.
-        </h1>
+        <h2 className="text-4xl md:text-6xl font-bold mb-4">
+          Καλώς ήρθες στο πρόγραμμα διαχείρησης κοινότητας.
+        </h2>
+        <h2 className="text-4xl md:text-6xl font-bold mb-4">
+          Αποφάσισε για την περιοχή σου.
+        </h2>
         <p className="text-xl md:text-2xl mb-8">
           Η αξιόπιστη πηγή σας για τις πιο πρόσφατες ειδήσεις και άρθρα.
         </p>
