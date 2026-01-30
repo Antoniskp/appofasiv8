6bb6336df5dd435ba41ac4becdaacee3e187d7dc
Antonis <antoniskp@gmail.com>
2026-01-30 19:46:20 +0200

    Change heading tags from h2 to h3

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -7,12 +7,12 @@ export default function HomeHero() {
   return (
     <section className="bg-blue-600 text-white py-20">
       <div className="app-container text-center">
-        <h2 className="text-4xl md:text-6xl font-bold mb-4">
+        <h3 className="text-4xl md:text-6xl font-bold mb-4">
           Καλώς ήρθες στο πρόγραμμα διαχείρησης κοινότητας.
-        </h2>
-        <h2 className="text-4xl md:text-6xl font-bold mb-4">
+        </h3>
+        <h3 className="text-4xl md:text-6xl font-bold mb-4">
           Αποφάσισε για την περιοχή σου.
-        </h2>
+        </h3>
         <p className="text-xl md:text-2xl mb-8">
           Η αξιόπιστη πηγή σας για τις πιο πρόσφατες ειδήσεις και άρθρα.
         </p>
