757a3805a9a5eb542de552a091bb788d90041123
Antonis <antoniskp@gmail.com>
2026-01-31 10:47:37 +0200

    Change heading tags from h3 to h4

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -7,12 +7,12 @@ export default function HomeHero() {
   return (
     <section className="bg-blue-600 text-white py-20">
       <div className="app-container text-center">
-        <h3 className="text-4xl md:text-6xl font-bold mb-4">
+        <h4 className="text-4xl md:text-6xl font-bold mb-4">
           Καλώς ήρθες στο πρόγραμμα διαχείρησης κοινότητας.
-        </h3>
-        <h3 className="text-4xl md:text-6xl font-bold mb-4">
+        </h4>
+        <h4 className="text-4xl md:text-6xl font-bold mb-4">
           Αποφάσισε για την περιοχή σου.
-        </h3>
+        </h4>
         <p className="text-xl md:text-2xl mb-8">
           Η αξιόπιστη πηγή σας για τις πιο πρόσφατες ειδήσεις και άρθρα.
         </p>
